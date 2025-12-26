cfb5fce18fc8918c12742ef3d517ffbfc2d896e8
aurelien <aurelien@toovalu.com>
2025-12-26 09:48:17 +0100

    fix: rector

--- src/Model/Translatable/TranslationMethodsTrait.php
+++ src/Model/Translatable/TranslationMethodsTrait.php
@@ -48,7 +48,7 @@ trait TranslationMethodsTrait
                 continue;
             }
 
-            if (\is_string($value) && \strlen(\trim($value)) > 0) {
+            if (\is_string($value) && \trim($value) !== '') {
                 return false;
             }
 
